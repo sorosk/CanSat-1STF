f2a240826dbcf4ac9565955d72fe268f251d844e
Daddahu <70315095+sorosk@users.noreply.github.com>
2020-12-16 21:38:12 +0100

    Test

--- CanSat/CanSat.ino
+++ CanSat/CanSat.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-
+  serial.open(9600)
+  int x = 2;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if x == 2 {
+    serial.write(x);
+  }
 }
